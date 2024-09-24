2ddfce30ab61486b1da07476b2d2d97d8b50539a
mob-psychic <anthonygabrieldeoliveita@gmail.com>
2024-09-24 11:12:33 -0300

    segundo projeto

---
+++ letras.c
@@ -0,0 +1,12 @@
+#include <stdio.h>;
+int main(){
+    int numero1 = 6;
+    int numero2 = 20;
+    int soma;
+    char letra = '1';
+    char nome[7]={"anthony"};
+    printf("%c\n", letra);
+    soma = numero1 + numero2;
+    printf("%d", soma);
+    return 0;
+}
