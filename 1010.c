d7f1fdf691f22788ee801adcf93286927b447ace
mob-psychic <anthonygabrieldeoliveita@gmail.com>
2024-10-08 09:34:30 -0300

    Add files via upload

---
+++ 1010.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+    int idadeAnthony = 17;
+    int idadeFelipe = 18;
+
+    if( idadeAnthony > idadeFelipe){
+        printf(" Anthony e mais velho que Felipe");
+    } 
+    else printf("Felipe e mais velho que Anthony");
+
+}
