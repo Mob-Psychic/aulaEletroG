8341afc4f208c7166c0b5c9b31894c1fc1bcf448
mob-psychic <anthonygabrieldeoliveita@gmail.com>
2024-10-08 08:19:49 -0300

    Add files via upload

---
+++ 007.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    float a, b, c
+    printf("digite sua peso:");
+    scanf("%f , &a");
+    printf("digite sua altura em cm");
+    scanf("%f , &b");
+    c=a/(b*b);
+    printf("seu imc e:%.1f")
+    return 0
+}
